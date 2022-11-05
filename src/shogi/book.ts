f9ff04d13191520e811118929c6436c47a465612
Koki Fushimi <paalon1936@gmail.com>
2022-11-05 18:42:53 +0900

    Add simple book class.

--- src/shogi/book.ts
+++ src/shogi/book.ts
@@ -2,6 +2,48 @@
 // 一般の有向グラフを実装するライブラリがあるならば、それを使ったほうが良いか？
 // 後に shogi 以下を独立したライブラリとして分離するならば、どこまでの機能をここで実装すればよいか？
 // 広まっているチェスのライブラリなども参考にしたい。
+
+import Graph from "graphology";
+
+export class Book {
+    graph = new Graph();
+    constructor() {
+    }
+    addNode(sfen: string) {
+        this.graph.addNode(sfen);
+    }
+    dropNode(sfen: string) {
+        this.graph.dropNode(sfen);
+    }
+    setNodeLabel(sfen: string, label: string) {
+        this.graph.setNodeAttribute(sfen, "label", label);
+    }
+    getNodeLabel(sfen: string): string {
+        return this.graph.getNodeAttribute(sfen, "label");
+    }
+    setNodeWeight(sfen: string, weight: number) {
+        this.graph.setNodeAttribute(sfen, "weight", weight);
+    }
+    getNodeWeight(sfen: string): number {
+        return this.graph.getNodeAttribute(sfen, "weight");
+    }
+    addEdge(source: string, target: string, label: string) {
+        this.graph.addEdge(source, target, {
+            label: label,
+            weight: 1
+        });
+    }
+    dropEdge(source: string, target: string) {
+        this.graph.dropEdge(source, target);
+    }
+    export() {
+        this.graph.export();
+    }
+    import(data: any) {
+        this.graph.import(data);
+    }
+}
+
 // export class BookMetadata {
     
 // }
