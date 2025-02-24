92c198275f9502ad88e4ed4d4520207cfc8d987c
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2025-02-24 23:58:47 +0900

    fix

--- src/renderer/view/primitive/BoardGrid.vue
+++ src/renderer/view/primitive/BoardGrid.vue
@@ -1,107 +1,44 @@
 <template>
-   <svg width="439.21539mm" height="479.79199mm" viewBox="0 0 439.21539 479.79199" xmlns="http://www.w3.org/2000/svg">
-  <g transform="translate(-225.09593,-247.17041)">
-    <g transform="matrix(0.49900175,0,0,0.49800041,222.93016,244.53834)">
-      <rect class="rect" x="17.170895"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23779"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30469"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30469"  y="121.93546"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170893"  y="121.93546"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23779"  y="121.93546"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30469"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170895"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23779"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37158"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43848"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.50537"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.50537"  y="121.93547"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37158"  y="121.93547"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43848"  y="121.93547"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.50537"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37158"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43848"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57227"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63916"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="17.594574"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="121.93546"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57227"  y="121.93546"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63916"  y="121.93546"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57227"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63916"  y="226.27634"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170898"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23779"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30469"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30469"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170898"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23779"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30469"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170898"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23779"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37158"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37158"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37158"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57227"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63916"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="330.61722"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57227"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63916"  y="434.9581"   width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57227"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63916"  y="539.29901"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170929"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23782"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30472"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30472"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170929"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23782"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30472"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170929"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23782"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37161"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37161"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30472"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170929"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23782"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37161"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57233"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63922"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="643.63989"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57233"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63922"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="207.30472"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="17.170929"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="112.23782"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="302.37161"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="397.43851"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="492.5054"   y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57233"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63922"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="852.32166"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="777.70605"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="587.57233"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <rect class="rect" x="682.63922"  y="747.98077"  width="95.066902" height="104.34088"/>
-      <circle class="circle" cx="587.60284" cy="330.60928" r="6.1868367"/>
-      <ellipse class="circle" cx="302.2171" cy="330.72778" rx="6.1867518" ry="6.1868367"/>
-      <circle class="circle" cx="587.80469" cy="643.56665" r="6.1868367"/>
-      <circle class="circle" cx="302.41901" cy="643.68518" r="6.1868367"/>
+  <svg
+    width="439.21539mm"
+    height="479.79199mm"
+    viewBox="0 0 439.21539 479.79199"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g transform="translate(-225.09593,-247.17041)">
+      <g transform="matrix(0.49900175,0,0,0.49800041,222.93016,244.53834)">
+        <g :style="`stroke: ${color}; stroke-width: 2`">
+          <!-- 外枠 -->
+          <rect x="17.170895" y="17.594574" width="855.602118" height="939.067966" fill="none" />
+          <!-- 縦線 -->
+          <line x1="112.23779" y1="17.594574" x2="112.23779" y2="956.66254" />
+          <line x1="207.30469" y1="17.594574" x2="207.30469" y2="956.66254" />
+          <line x1="302.37158" y1="17.594574" x2="302.37158" y2="956.66254" />
+          <line x1="397.43848" y1="17.594574" x2="397.43848" y2="956.66254" />
+          <line x1="492.50537" y1="17.594574" x2="492.50537" y2="956.66254" />
+          <line x1="587.57227" y1="17.594574" x2="587.57227" y2="956.66254" />
+          <line x1="682.63916" y1="17.594574" x2="682.63916" y2="956.66254" />
+          <line x1="777.70605" y1="17.594574" x2="777.70605" y2="956.66254" />
+          <!-- 横線 -->
+          <line x1="17.170895" y1="121.93546" x2="872.773013" y2="121.93546" />
+          <line x1="17.170895" y1="226.27634" x2="872.773013" y2="226.27634" />
+          <line x1="17.170895" y1="330.61722" x2="872.773013" y2="330.61722" />
+          <line x1="17.170895" y1="434.9581" x2="872.773013" y2="434.9581" />
+          <line x1="17.170895" y1="539.29901" x2="872.773013" y2="539.29901" />
+          <line x1="17.170895" y1="643.63989" x2="872.773013" y2="643.63989" />
+          <line x1="17.170895" y1="747.98077" x2="872.773013" y2="747.98077" />
+          <line x1="17.170895" y1="852.32166" x2="872.773013" y2="852.32166" />
+        </g>
+        <g :style="`fill: ${color}; stroke: none`">
+          <!-- 3マス間隔の印 -->
+          <circle cx="587.60284" cy="330.60928" r="6.1868367" />
+          <circle cx="302.2171" cy="330.72778" r="6.1868367" />
+          <circle cx="587.80469" cy="643.56665" r="6.1868367" />
+          <circle cx="302.41901" cy="643.68518" r="6.1868367" />
+        </g>
+      </g>
     </g>
-  </g>
-</svg>
+  </svg>
 </template>
 
 <script setup lang="ts">
@@ -112,16 +49,3 @@ defineProps({
   },
 });
 </script>
-
-<style scoped>
-.rect {
-  fill: none;
-  stroke: #000;
-  stroke-width: 2.006;
-  stroke-linecap: square;
-  stroke-miterlimit: 4;
-}
-.circle {
-  fill: #000;
-}
-</style>
