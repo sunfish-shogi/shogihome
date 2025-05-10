45b5449483ab236cd0f0c5e7410111047e6d8607
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2025-05-10 19:03:40 +0900

    Revert "Fix for double tap"
    This reverts commit b4a7fdeeaf86332b07922c7960145a4d96f36d38.

--- src/renderer/view/primitive/BoardView.vue
+++ src/renderer/view/primitive/BoardView.vue
@@ -69,7 +69,7 @@
           :key="square.id"
           :style="square.style"
           @click.stop.prevent="clickSquare(square.file, square.rank)"
-          @dblclick.stop.prevent="clickSquareDouble(square.file, square.rank)"
+          @dblclick.stop.prevent="clickSquareR(square.file, square.rank)"
           @contextmenu.stop.prevent="clickSquareR(square.file, square.rank)"
         ></div>
         <div
@@ -444,14 +444,6 @@ const clickSquareR = (file: number, rank: number) => {
   const square = new Square(file, rank);
   if (props.allowEdit && props.position.board.at(square)) {
     emit("edit", { rotate: square });
-  }
-};
-
-const clickSquareDouble = (file: number, rank: number) => {
-  if (props.allowEdit) {
-    clickSquareR(file, rank);
-  } else {
-    clickSquare(file, rank);
   }
 };
 
