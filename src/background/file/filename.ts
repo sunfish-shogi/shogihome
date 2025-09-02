f35d53ef982d9368d212fcf2c02e2a5a8ddffa66
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2025-09-03 00:43:44 +0900

    Add limitter

--- src/background/file/filename.ts
+++ src/background/file/filename.ts
@@ -5,6 +5,9 @@ export async function resolveConflictFilePath(filePath: string): Promise<string>
   const parsed = path.parse(filePath);
   let suffix = 2;
   while (await exists(filePath)) {
+    if (suffix > 1000) {
+      throw new Error("Too many files with the same name");
+    }
     filePath = path.join(parsed.dir, parsed.name + "-" + suffix + parsed.ext);
     suffix++;
   }
