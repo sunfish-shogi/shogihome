3d45b339805f9599cb1a7c7c215259bcbabacb41
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2026-01-12 01:10:19 +0900

    Fix isBookUnsaved

--- src/background/book/index.ts
+++ src/background/book/index.ts
@@ -111,7 +111,7 @@ function emptyBook(): BookHandle {
 let book: BookHandle = emptyBook();
 
 export function isBookUnsaved(): boolean {
-  return book.type === "in-memory" && !book.saved;
+  return !book.saved;
 }
 
 export function getBookFormat(): BookFormat {
